48d2fae9b0e68c307107fb6ff77e5b90175553d6
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-07-16 17:25:25 -0400

    Add number button

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const NumberButton = props => {
+    const { number } = props;
+    return (
+        <>
+            {/* Display a button element rendering the data being passed down from the parent container on props */}
+            <button>{number}</button>
+        </>
+    );
 };
